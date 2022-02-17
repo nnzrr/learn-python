ddd7808ac3d0c59c7aab16ce27faacf06da97deb
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-02-17 20:47:34 +0700

    Update 02.py

--- 02.py
+++ 02.py
@@ -4,3 +4,19 @@ for c in k:
         print(f'{c} blast.')
     else:
         print(f'{c} rica.')
+        
+        
+        
+x = range(1,101)
+k = ['aba','uku','ele']
+for ce in k:
+    if 'uku' == ce:
+        print(f"j {ce}")
+    elif 'aba' == ce:
+        print(f"euy {ce}")
+    else:
+        for z in x:
+            if z % 3 and z % 5 and z > 5:
+                print(f'{z} tidak habis dibagi tiga atau lima.')
+            else:
+                print(f'wakwaw {z}')
